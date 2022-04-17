8aae2863069dd5c2d6a21484dd2b38f620675470
Tyson <tysn.xu@gmail.com>
2022-04-17 17:54:33 +0800

    Shot 16 Update

--- Assets/Obscurio Scene/!!! Scenes for Collaboration/Scene 16-22 (Tyson)/Scene Scripts/shot16_boyHangingIK.cs
+++ Assets/Obscurio Scene/!!! Scenes for Collaboration/Scene 16-22 (Tyson)/Scene Scripts/shot16_boyHangingIK.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class shot16_boyHangingIK : MonoBehaviour
 {
+    Animator boyAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boyAnimator = GetComponent<Animator>();
     }
+
+    
 
     // Update is called once per frame
     void Update()
