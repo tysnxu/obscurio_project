316b8645c5e6fbf90415983a3216857c830e4b79
Tyson <tysn.xu@gmail.com>
2022-04-21 00:23:16 +0800

    Shot 16 - Added basic boy animation

--- Assets/Obscurio Scene/!!! Scenes for Collaboration/Scene 16-22 (Tyson)/Scene Scripts/shot16_boyHangingIK.cs
+++ Assets/Obscurio Scene/!!! Scenes for Collaboration/Scene 16-22 (Tyson)/Scene Scripts/shot16_boyHangingIK.cs
@@ -6,12 +6,18 @@ public class shot16_boyHangingIK : MonoBehaviour
 {
     Animator boyAnimator;
     // Start is called before the first frame update
+    public GameObject boyArmIKTarget;
+    public float boyArmIKWeight = 1.0f;
+
     void Start()
     {
         boyAnimator = GetComponent<Animator>();
     }
 
-    
+    private void OnAnimatorIK(int layerIndex) {
+        boyAnimator.SetIKPosition(AvatarIKGoal.LeftHand, boyArmIKTarget.transform.position);
+        boyAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, boyArmIKWeight);
+    }
 
     // Update is called once per frame
     void Update()
